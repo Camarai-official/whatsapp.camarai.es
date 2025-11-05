ee143ae7c16de1f6f7bbcb38b4a165013805d214
franciscocamarai <fran@camarai.es>
2025-11-05 20:38:42 +0100

    removed border section in responisve

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -175,7 +175,7 @@ export default function Slide({
       className="
         relative w-full max-w-[1200px] xl:h-[87vh]
         px-4 xl:px-22 py-12 xl:py-8
-        rounded-4xl border border-slate-300/30 bg-slate-950/60 backdrop-blur-xl
+        rounded-4xl border-none sm:border border-slate-300/30 bg-slate-950/60 backdrop-blur-xl
         flex flex-col items-center justify-center gap-6
       "
     >
