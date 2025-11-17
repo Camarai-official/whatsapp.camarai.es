fa7fcdf4e393d4f40fb0332e73aab8a316b99142
Miguelriu.Dev <miguelriu.dev@gmail.com>
2025-11-17 19:10:24 +0100

    Deleted a reference call

--- src/pages/FinalPage.jsx
+++ src/pages/FinalPage.jsx
@@ -4,8 +4,6 @@ import { Icon } from "@iconify/react"
 import { useTranslation } from "../hooks/useTranslation.jsx"
 
 import Img from "../assets/camarai_rocket.webp"
-import Img1 from "../assets/1.webp"
-import Img2 from "../assets/2.webp"
 import Img7 from "../assets/7.webp"
 import Img9 from "../assets/9.webp"
 
@@ -14,21 +12,6 @@ export default function FinalPage({ onSlide }) {
 
   // === DATA: CARDS ===
   const cards = [
-    {
-      img: Img1,
-      title: t("finalPage.cards.card1.title"),
-      desc: t("finalPage.cards.card1.desc"),
-    },
-    {
-      img: Img9,
-      title: t("finalPage.cards.card2.title"),
-      desc: t("finalPage.cards.card2.desc"),
-    },
-    {
-      img: Img2,
-      title: t("finalPage.cards.card3.title"),
-      desc: t("finalPage.cards.card3.desc"),
-    },
     {
       img: Img7,
       title: t("finalPage.cards.card4.title"),
